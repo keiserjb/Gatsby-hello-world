c39587cc15bc85275b7b6955132e6640a62a9333
Justin Keiser <JustinK@modelaircraft.org>
2020-10-21 14:24:03 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/about/">About </link>   
+      <Link to="/about/">About</link>   
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
